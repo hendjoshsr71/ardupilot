f4a7151d286d404b94de5f2fd569dd7f64e582a9
EMJ-259 <77213386+EMJ-259@users.noreply.github.com>
2021-01-10 16:00:03 -0600

    Update plane_sim.sh

--- libraries/SITL/examples/test/plane_sim.sh
+++ libraries/SITL/examples/test/plane_sim.sh
@@ -24,12 +24,12 @@ cat <<EOF > swarm/plane2/follow.parm
 SYSID_THISMAV 2
 EOF
 
-(cd swarm/plane2 && $PLANE --model plane --home -35.3632627,149.165388867259,584.872916847261,0 --uartA $UARTA --instance 1 --defaults $PLANE_DEFAULTS,follow.parm) &
+(cd swarm/plane2 && $PLANE --model plane --home -35.3632627,149.165388867259,584.872916847261,0 --uartA $UARTA --instance 1 --defaults $PLANE_DEFAULTS) &
 
 # create default parameter file for the follower 2
 cat <<EOF > swarm/plane3/follow.parm
 SYSID_THISMAV 3
 EOF
 
-(cd swarm/plane3 && $PLANE --model plane --home -35.3632627,149.164973795414,584.926584510997,0 --uartA $UARTA --instance 2 --defaults $PLANE_DEFAULTS,follow.parm) &
+(cd swarm/plane3 && $PLANE --model plane --home -35.3632627,149.164973795414,584.926584510997,0 --uartA $UARTA --instance 2 --defaults $PLANE_DEFAULTS) &
 wait
