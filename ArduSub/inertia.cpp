d536fd60790a77b3b488e48acbe8b071363acd13
Josh Henderson <hendjoshsr71@gmail.com>
2021-06-21 17:55:02 -0400

    ArduSub: rename get_velocity().z to climb_rate()

--- ArduSub/inertia.cpp
+++ ArduSub/inertia.cpp
@@ -21,5 +21,5 @@ void Sub::read_inertia()
 
     // get velocity, altitude is always absolute frame, referenced from
     // water's surface
-    climb_rate = inertial_nav.get_velocity_z();
+    climb_rate = inertial_nav.get_climb_rate();
 }
