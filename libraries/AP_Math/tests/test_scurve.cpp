46107a2bc53ff41bf962d552525f51aa7b4f8d37
Josh Henderson <hendjoshsr71@gmail.com>
2021-07-10 00:59:37 -0400

    test_scurve

--- libraries/AP_Math/tests/test_scurve.cpp
+++ libraries/AP_Math/tests/test_scurve.cpp
@@ -24,15 +24,47 @@ TEST(LinesScurve, test_calculate_path)
 // Test that no velocity change returns
 TEST(LinesScurve, test_calculate_path)
 {
-    float Jm_out = 1, t2_out = 2, t4_out = 3, t6_out = 4;
+    float Jm_out = 1.1, t2_out = 2.2, t4_out = 3.3, t6_out = 4.4;
     SCurve::calculate_path(0.0, 0.0, 10.0, 0.0, 1.0, 2.09772229,
                            Jm_out, t2_out, t4_out, t6_out);
-    // How to check 
-    EXPECT_FLOAT_EQ(Jm_out, 1.0);
-    EXPECT_FLOAT_EQ(t2_out, 2.0);
-    EXPECT_FLOAT_EQ(t4_out, 3.0);
-    EXPECT_FLOAT_EQ(t6_out, 4.0);
+    // How to check if it returns immeadiately before setting the floats without bool output
+    EXPECT_FLOAT_EQ(Jm_out, 1.1);
+    EXPECT_FLOAT_EQ(t2_out, 2.2);
+    EXPECT_FLOAT_EQ(t4_out, 3.2);
+    EXPECT_FLOAT_EQ(t6_out, 4.4);
 }
+
+// Test solution = 0 - t6 t4 t2 = 0 0 0
+// Test (fabsf(Am) < Jm * tj): TRUE && (Vm <= V0 + 2.0f * Am * tj) = TRUE
+
+
+
+
+// Test solution = 0 - t6 t4 t2 = 0 0 0
+// Test (fabsf(Am) < Jm * tj): TRUE && (L <= 4.0f * V0 * tj + 4.0f * Am * sq(tj)) = TRUE
+
+
+
+// Test solution = 2 - t6 t4 t2 = 0 1 0
+// Test (fabsf(Am) < Jm * tj) && (Vm <= V0 + 2.0f * Am * tj) = FALSE && (L <= 4.0f * V0 * tj + 4.0f * Am * sq(tj)) = FALSE
+
+
+// solution = 5 - t6 t4 t2 = 1 0 1
+// Test (fabsf(Am) < Jm * tj): FALSE  && (0 < a1) : TRUE
+
+
+
+// solution = 5 - t6 t4 t2 = 1 0 1
+// Test (fabsf(Am) < Jm * tj): FALSE  && (L < 1.0f / (Jm_sq) * (Am_sq * Am + Am * Jm * (V0 * 2.0f + Am * tj * 2.0f)) + V0 * tj * 2.0f + Am * (tj_sq)) : TRUE
+
+
+// solution = 7 - t6 t4 t2 = 1 1 1
+// Test (fabsf(Am) < Jm * tj): FALSE  && (0 < a1) : FALSE
+// && (L < 1.0f / (Jm_sq) * (Am_sq * Am + Am * Jm * (V0 * 2.0f + Am * tj * 2.0f)) + V0 * tj * 2.0f + Am * (tj_sq)) : TRUE
+
+
+
+
 
 
 AP_GTEST_MAIN()
