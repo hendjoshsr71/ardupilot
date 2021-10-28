857cc15579d60a9c7e68e3955d0ea57667b0be87
Josh Henderson <hendjoshsr71@gmail.com>
2021-10-28 15:15:48 -0400

    ahrs rebase

--- libraries/AP_AHRS/AP_AHRS_Backend.cpp
+++ libraries/AP_AHRS/AP_AHRS_Backend.cpp
@@ -321,7 +321,7 @@ void AP_AHRS::Log_Write_Home_And_Origin()
 // get apparent to true airspeed ratio
 float AP_AHRS_Backend::get_EAS2TAS(void) const {
     // FIXME the rebase here is convoluted.....
-    return _EAS2TAS;
+    return AP::baro().get_EAS2TAS();
 }
 
 // return current vibration vector for primary IMU
