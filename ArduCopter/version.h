f50f0779d9312172644e9d3093f964a730459760
Andy Piper <github@andypiper.com>
2024-12-12 09:14:23 +0000

    Copter: update version to reflect custom version

--- ArduCopter/version.h
+++ ArduCopter/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduCopter V4.6.0"
+#define THISFIRMWARE "ArduCopter V4.6.0+"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,0,FIRMWARE_VERSION_TYPE_OFFICIAL
